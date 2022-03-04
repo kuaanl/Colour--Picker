a6c98312b0e9bfeca42131f6e28b67e190d56b62
kuaanl <94217846kunal@gmail.com>
2022-03-04 06:27:02 +0530

    Colour Picker

---
+++ color.js
@@ -0,0 +1,118 @@
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
+var square = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var rgbCode = document.getElementById("rgbCode");
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+easyBtn.addEventListener("click", function () {
+  // class list  to remove froom default hard
+   hardBtn.classList.remove("selected");
+   // tranfer the class 
+
+   easyBtn.classList.add("selected");
+  numSquares = 3;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  rgbCode.textContent = pickedColor;
+  for (var i = 0; i < square.length; i++) {
+    if (colors[i]) {
+      square[i].style.background = colors[i];
+    } else {
+      square[i].style.display = "none";
+    }
+  }
+});
+
+hardBtn.addEventListener("click", function () {
+  easyBtn.classList.remove("selected");
+  hardBtn.classList.add("selected");
+  numSquares = 6;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  rgbCode.textContent = pickedColor;
+  for (var i = 0; i < square.length; i++) {
+    square[i].style.background = colors[i];
+    square[i].style.display = "block";
+  }
+});
+
+resetButton.addEventListener("click", function () {
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  rgbCode.textContent = pickedColor;
+  this.textContent = "New Color";
+  messageDisplay.textContent = "";
+  for (var i = 0; i < square.length; i++) {
+    square[i].style.background = colors[i];
+  }
+  h1.style.background = "steelblue";
+});
+
+
+for(var i=0;i<square.length;i++)
+{
+    square[i].style.background = colors[i];
+   square[i].addEventListener("click",function(){
+        
+    var clickedColor=this.style.background;
+    if(clickedColor===pickedColor)
+    {
+            //rgbCode.textContent=" Well Done :) ";
+            resetButton.textContent="Play Again ??";
+            messageDisplay.textContent="Well Done :)"
+            changeColors(clickedColor);
+            h1.style.background = clickedColor;
+    }
+
+    else
+    {
+                 this.style.background="#848cb9";
+                 messageDisplay.textContent="Try Again :("
+                   
+    }
+
+   })
+}
+
+function changeColors(color)
+{
+  for (var i = 0; i < square.length; i++) 
+  {
+    square[i].style.background = color;
+  }
+}
+
+
+
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+}
+
+function generateRandomColors(num) {
+
+  var arr = [];
+  for (var i = 0; i < num; i++) {
+    arr.push(randomColor());
+    
+  }
+  return arr;
+}
+
+function randomColor() {
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+
+  return "rgb(" + r + ", " + g + ", " + b + ")";
+}
+
+
+
+
+
